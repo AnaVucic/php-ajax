1ab2af0cafa941fbd96a23709caf633536129b5e
Ana Vucic <anavucic007@gmail.com>
2023-02-03 16:16:23 +0100

    script moved from home to main.js

---
+++ js/main.js
@@ -0,0 +1,114 @@
+$(document).ready(function() {
+    console.log("Page is ready!");
+    const now = new Date();
+  });
+
+  $("#addForm").submit(function() {
+    console.log("nesto");
+    event.preventDefault();
+    const $form = $(this);
+
+    // const $fromspan = $form.find("span");
+    // console.log($fromspan);
+    // var value = $("#myDate").datepicker("getDate");
+    // console.log(value);
+
+    const $input = $form.find("input, select");
+    // $input.add(value);
+    // console.log($input);
+    const serializedData = $form.serialize();
+    console.log(serializedData);
+
+    $input.prop('disabled', true);
+
+    request = $.ajax({
+      url: 'handler/add.php',
+      type: 'post',
+      data: serializedData
+    });
+
+    request.done(function(response) {
+      console.log(response);
+      if (response == "Success") {
+        console.log("Added new membership");
+        location.reload(true);
+      } else {
+        console.log("Membership not added " + response);
+      }
+      console.log(response);
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+      console.error("Error occurred: " + textStatus, errorThrown);
+    });
+  });
+
+
+  $(".edit-membership").click(function() {
+
+    var compositeKey = $(this).val();
+    console.log("Filling edit form");
+    console.log(compositeKey);
+    const keys = compositeKey.split(' ');
+
+    request = $.ajax({
+      url: 'handler/get.php',
+      type: 'post',
+      data: {
+        'key1': keys[0],
+        'key2': keys[1]
+      },
+      dataType: 'json',
+      error: function(response) {
+        console.log(response);
+      }
+    });
+
+    request.done(function(response) {
+      console.log('Form filled');
+      console.log(response);
+
+      $('#memberNameEdit').val(response.member.firstname + " " + response.member.lastname);
+      $('#memberIDEdit').val(response.member.memberid);
+      $('#membershipNameEdit').val(response.membership.membershipname);
+      $('#membershipIDEdit').val(response.membership.membershipid);
+
+      var date = new Date(response.startDate.date);
+      console.log(date);
+      document.getElementById('editDate').value = date.toLocaleDateString("en-US", {year: "numeric", month: "2-digit", day: "2-digit"});
+
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+      console.error('The following error occurred: ' + textStatus, errorThrown);
+    });
+
+  });
+
+  $('#editForm').submit(function() {
+    event.preventDefault();
+    console.log("Editing");
+    const $form = $(this);
+    const $inputs = $form.find('input');
+    const serializedData = $form.serialize();
+    console.log(serializedData);
+    $inputs.prop('disabled', true);
+
+    request = $.ajax({
+      url: 'handler/edit.php',
+      type: 'post',
+      data: serializedData
+    });
+
+    request.done(function(response) {
+      if (response === 'Success') {
+        console.log('Appointment edited');
+        location.reload(true);
+      } else console.log('Appointment not edited ' + response);
+      console.log(response);
+    });
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+      console.error('The following error occurred: ' + textStatus, errorThrown);
+    });
+  });
